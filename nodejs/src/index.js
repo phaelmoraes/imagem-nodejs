df41db588f3a75ee7789514341c37ce71c180d80
Raphael Moraes <Antnraphael@gmail.com>
2021-03-01 10:47:51 -0300

    feat: Implementação de script de criação da tabela people

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -12,44 +12,56 @@ const credentials = {
 
 const connection = mysql.createConnection(credentials);
 
-const storePeople = async (people) => {
+const createPeopleTable  = async () => {
   return new Promise(async resolve=>{
-    await connection.connect((err) => {
-      const sql = `INSERT INTO people(name) VALUES ?`;
-      connection.query(sql,[people], function (error) {
-        if (error) throw error;
-        resolve()
-      });
+    const sql =
+    "CREATE TABLE IF NOT EXISTS mydb.people ( id integer not null auto_increment primary key, name VARCHAR(255));";
+    await connection.query(sql, function (err, result) {
+      if (err) console.log(err);
+      resolve();
+    });
+  })
+}
+
+
+const storePeople = async (people) => {
+  return new Promise(async (resolve) => {
+    const sql = `INSERT INTO people(name) VALUES ?`;
+    await connection.query(sql, [people], function (error) {
+      if (error) throw error;
+      resolve();
     });
   });
 };
 
 const getPeople = async () => {
-  return new Promise(async resolve=>{
+  return new Promise(async (resolve) => {
     const sql = "SELECT name FROM people";
     await connection.query(sql, function (error, results) {
       if (error) throw error;
-      resolve(results)
+      resolve(results);
     });
-  })
+  });
 };
 
 app.get("/", async (req, res) => {
+  let people = [["Raphael Moraes"]];
   let html = `<h1>Full Cycle Rocks!</h1><ul>`;
-  let people = [
-    ["Raphael Moraes"]
-  ];
 
-  await storePeople(people).then(async ()=>{
-    await getPeople().then(people=> {
-      people.forEach((p) => {
-        html += `<li>${p.name}</li>`;
-      });
+  await Promise.all([
+    createPeopleTable(), 
+    storePeople(people)
+  ]);
+
+  await getPeople().then((people) => {
+    people.forEach((p) => {
+      html += `<li>${p.name}</li>`;
     });
   });
+  
 
   html += `</ul>`;
-  
+
   res.send(html);
 });
 
